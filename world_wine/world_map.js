8411a2dbaffbfb1c7065812a007d03b9ae3efaf9
Robert Imthurn <rpimthurn@yahoo.com>
2020-03-18 21:36:20 -0500

    world wine initial commit

---
+++ world_wine/world_map.js
@@ -0,0 +1,52 @@
+Plotly.d3.csv("/wine_production.csv", function(err, rows){
+    function unpack(rows, key) {
+        return rows.map(function(row) { return row[key]; });
+        }
+    
+      console.log(unpack(rows, 'CODE'))
+      console.log(unpack(rows, 'WineProduction[HCL]'))
+      console.log(unpack(rows, 'Country'))
+
+        let data = [{
+            type: 'choropleth',
+            locations: unpack(rows, 'CODE'),
+            z: unpack(rows, 'WineProduction[HCL]'),
+            text: unpack(rows, 'Country'),
+            colorscale: [
+                [0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],
+                [0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],
+                [0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']],
+            autocolorscale: false,
+            reversescale: true,
+            marker: {
+                line: {
+                    color: 'rgb(180,180,180)',
+                    width: 0.5
+                    }
+                },
+        
+            tick0: 0,
+            zmin: 0,
+            dtick: 1000,
+            colorbar: {
+                autotic: false,
+                tickprefix: '$',
+                title: 'GDP<br>Billions US$'
+            }
+        }]
+        var layout = {
+          title: '2014 Global GDP<br>Source: <a href="https://www.cia.gov/library/publications/the-world-factbook/fields/2195.html"> CIA World Factbook</a>',
+          geo:{
+              showframe: false,
+              showcoastlines: false,
+              projection:{
+                  type: 'mercator'
+              }
+          }
+        }
+        Plotly.newPlot("map", data, layout, {showLink: false});
+})
+
+
+
+
