fcc7bd59d6dccac58c9c361ce152bbc028cb0610
gabprado <gabprado@hotmail.com>
2020-05-04 20:18:22 -0500

    add radar plot js file

--- Website/static/JS/wine_data.js
+++ Website/static/JS/wine_data.js
@@ -1,25 +1,34 @@
 function renderPlot(wine){
     d3.json(`wine_data/${wine}`).then((wine_data) => {
         let data = [{
-            values: wine_data.Attribute_Values,
-            labels: wine_data.Attribute_Labels,
-            text: wine_data.Attribute_Labels,
-            textinfo: "text",
-            showlegend : false,
-            hole: .6,
-            type: 'pie'
+          type: "scatterpolar",
+          r: wine_data.Attribute_Values,
+          theta: wine_data.Attribute_Labels,
+          line: {
+             color: "#000000",
+             width: 3
+          },
+          mode: "markers",
+          fill: "toself",
+          fillcolor: "#DB3256",
+          hoverinfo: "r"
           }];
           
           let layout = {
+            polar: {
+              radialaxis: {
+                visible: true,
+              }
+            },
+            showlegend: false,
             height: 600,
             width: 600
 
           };
-          
           Plotly.newPlot('wine_plot', data, layout, {displayModeBar: false});
     })
 
 }
 
 
-renderPlot("Syrah")
+renderPlot("Malbec")
