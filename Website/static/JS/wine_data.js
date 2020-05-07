c8aa71d11523dad79d115ec8416f896de280296f
gabprado <gabprado@hotmail.com>
2020-05-06 23:07:33 -0500

    fix minor bug with d3 select and html cab sauv

--- Website/static/JS/wine_data.js
+++ Website/static/JS/wine_data.js
@@ -1,46 +1,3 @@
-// function renderPlot(wine){
-//     d3.json(`wine_data/${wine}`).then((wine_data) => {
-//         let data = [{
-//           type: "scatterpolar",
-//           r: wine_data.Attribute_Values,
-//           theta: wine_data.Attribute_Labels,
-//           line: {
-//              color: "#000000",
-//              width: 3
-//           },
-//           mode: "markers",
-//           fill: "toself",
-//           fillcolor: "#DB3256",
-//           hoverinfo: "r"
-//           }];
-          
-//           let layout = {
-//             polar: {
-//               radialaxis: {
-//                 visible: false,
-//               }
-//             },
-//             showlegend: false,
-//             height: 600,
-//             width: 600,
-//             // plot_bgcolor: "rgba(0,0,0,0)",
-//             paper_bgcolor: "rgba(0,0,0,0)",
-//             font:{
-//               size: 15,
-//               color: "white"
-//             }
-
-
-//           };
-//           Plotly.newPlot('wine_plot', data, layout, {displayModeBar: false});
-//     })
-
-// }
-
-
-// renderPlot("Malbec")
-
-
 function loadWineData(){
   wineClicked = d3.select(this).text()
   renderPlot(wineClicked)
@@ -83,4 +40,4 @@ function renderPlot(wine){
           Plotly.newPlot('wine_plot', data, layout, {displayModeBar: false});
     })
 }
-d3.selectAll("a").on("click", loadWineData)
+d3.selectAll(".dropdown-content").selectAll("a").on("click", loadWineData)
